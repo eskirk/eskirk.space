7cbe4f02de35d95df08683c1afdec9d15eb605c1
Elliot Kirk <elliot.s.kirk@gmail.com>
2018-03-30 00:25:08 -0700

    Added resume and sidebar menu

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,44 +1,56 @@
 import React, { Component } from 'react';
 import { Jumbotron, Row, Col } from 'react-bootstrap';
-import MarkdownRenderer from 'react-markdown-renderer';
 import './About.css';
+import { DeviceDvr } from 'material-ui';
 
 class About extends Component {
    constructor(props) {
       super(props);
-
-      this.markdown = '```js \nlet my = new ElliotKirk()' 
-      + '\n\nmy.interests \n> [\'artificial intelligence\', \'full-stack development\', ' 
-      + '\'data science\']'
-      + '\n\nmy.languages \n> [\'JavaScript\', \'Python\', \'Java\', \'PHP\', \'SQL\', \'C\']' 
-      + '\n\nmy.technologies \n> [\'React\', \'Git\', \'Node.js\']'
-      + '\n\nmy.hobbies \n> [\'slacklining\', \'climbing\', \'biking\']';
    }
    render() {
       return (
-         <section>
+         <div>
             <Row className='Title'>
-               <Col md={2}/>
-               <Col md={8}>
-                  <h4 className='AboutHeader'>
-                     Hello there, my name is Elliot Kirk
-                  </h4>
-               </Col>
-               <Col md={2}/>
+               <h4 className='AboutHeader'>
+                  Hello there, my name is Elliot Kirk
+               </h4>
             </Row>
+            <Code/>
+         </div>
+      )
+   }
+}
 
-         <Row>
-            <Col md={2}/>
-            <Col md={8}>
-            <Jumbotron className='Code'>
-               <Jumbotron className='About'>
-                  <MarkdownRenderer markdown={this.markdown}/>
-               </Jumbotron>
+class Code extends Component {
+   render() {
+      return (
+         <Jumbotron className='Code'>
+            <Jumbotron className='About'>
+               <code className='CodeBlock'>
+                  let my = new ElliotKirk()
+                  <br/><br/>
+                  my.interests 
+                  <br/>
+                  > ['artificial intelligence', 'full-stack development', 'data science']
+                  <br/><br/>
+                  my.hobbies 
+                  <br/>
+                  > ['slacklining', 'climbing', 'biking', 'coding']
+                  <br/><br/>
+                  my.education
+                  <br/>
+                  > 'Cal Poly San Luis Obispo - Computer Science'
+                  <br/><br/>
+                  my.graduation_date
+                  <br/>
+                  > 'Fall 2018'
+                  <br/><br/>
+                  my.resume 
+                  <br/>
+                  > <a target='_blank' href='./Resume.pdf' rel="noopener noreferrer">Resume.pdf</a>
+               </code>
             </Jumbotron>
-            </Col>
-            <Col md={2}/>
-         </Row>
-         </section>
+         </Jumbotron>
       )
    }
 }
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,45 +1,94 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Button, Row, Col } from 'react-bootstrap';
+import Hamburger from 'react-hamburgers';
+import { slide as Menu } from 'react-burger-menu'
 import World from '../Environment/World';
-import Content from '../Content/Content';
 import Projects from '../Projects/Projects';
 import About from '../About/About';
 import './Main.css';
+import './hamburgers.css';
 
 class Main extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         active: false,
+         hiddenWorld: false
+      }
+   }
+
+   isMenuOpen(state) {
+      this.setState({
+         active: state.isOpen
+      })
+   }
+
+   menuClicked(path) {
+      console.log(path);
+
+      if (path !== 'github')
+         this.props.history.push('/' + this.path);
+   }
+
    render() {
       return (
-         <div className='Main' ref={e => this.element = e}>
-            <header className='Main-header'>
-               <a href='http://eskirk.space' rel="noopener noreferrer">
-                  <h1 className='Main-title'>elliot kirk</h1>
-               </a>
-            </header>
-            <div>
-               <Switch>
-                  {/* Homepage */}
-                  <Route exact path='/' render={() =>
-                     <div className='AboutContainer'>
-                        <World {...this.props} />
+         <div id='MainWrapper'>
+            {/* Hidden sidebar menu */}
+            <Menu right={true}
+               isOpen={this.state.active}
+               pageWrapId={'Main'}
+               outerContainerId={'MainWrapper'}
+               onStateChange={this.isMenuOpen.bind(this)}>
+               <a target='_blank' href='https://github.com/eskirk' rel="noopener noreferrer">GitHub</a>
+               {/* <a href="#">Projects</a>
+               <Button onClick={ () => {
+                  this.setState({hiddenWorld: !this.state.hiddenWorld})
+                  console.log(this.state.hiddenWorld);
+               } }/> */}
+            </Menu>
+
+            <div className='Main' id='Main' ref={e => this.element = e}>
+               <header className='Main-header'>
+                  <Row>
+                     <Col md={6}>
+                        <a href='http://eskirk.space' rel="noopener noreferrer">
+                           <h1 className='Main-title'>elliot kirk</h1>
+                        </a>
+                     </Col>
+                     <Col md={6} className='Menu-button'>
+                        <Hamburger
+                           active={this.state.active}
+                           type="slider"
+                           onClick={() => this.setState({ active: !this.state.active })}
+                        />
+                     </Col>
+                  </Row>
+               </header>
+               <div>
+                  <Switch>
+                     {/* Homepage */}
+                     <Route exact path='/' render={() =>
+                        <div className='AboutContainer'>
+                           {!this.state.hiddenWorld ? <World {...this.props} /> : ''}
+                           <About {...this.props} />
+                        </div>}
+                     />
+
+                     {/* Projects page */}
+                     <Route path='/Projects' render={() =>
+                        <Projects {...this.props} />
+                     } />
+
+                     {/* About page */}
+                     <Route path='/About' render={() =>
                         <About {...this.props} />
-                        <Content {...this.props} />
-                     </div>}
-                  />
-
-                  {/* Projects page */}
-                  <Route path='/Projects' render={() => 
-                     <Projects {...this.props} />
-                  }/>
-
-                  {/* About page */}
-                  <Route path='/About' render={() => 
-                     <About {...this.props} />
-                  }/>
-               </Switch>
+                     } />
+                  </Switch>
+               </div>
             </div>
          </div>
-
-
       );
    }
 }
